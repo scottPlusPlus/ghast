df44ac15dcc3ebbc026188dda022621f0c22357e
scottPlusPlus <mail@scottplusplus.com>
2022-10-23 19:03:05 -0700

    simple options

--- index.js
+++ index.js
@@ -20,6 +20,11 @@ express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
+  .options('*', (req, res) => {
+    res.json({
+      status: 'OK'
+    });
+  })  
   .get('/', (req, res) => res.render('pages/index'))
   .get('/show', showData)
   .post('/push', receiveData)
