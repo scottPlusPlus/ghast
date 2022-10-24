40734ea835d0d41a6d7fc59aeaeb1ec40aa2e9d6
scottPlusPlus <mail@scottplusplus.com>
2022-10-23 18:51:19 -0700

    push and show data

--- index.js
+++ index.js
@@ -2,9 +2,22 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
+const data = [];
+
+const receiveData = (req, res)=> {
+  data.push(req.body);
+  res.status('200').json();
+}
+
+const showData = (req, res)=> {
+  res.status('200').json(data);
+}
+
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
+  .get('/show', showData)
+  .post('/push', receiveData)
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
